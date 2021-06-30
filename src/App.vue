9ccc893702eab33dc8c6c227ac6e120a293b36a9
Michell Brito <michellbrito@michells-mbp.myfiosgateway.com>
2021-06-30 10:38:58 -0400

    start

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <Cards />
+  </div>
+</template>
+
+<script>
+import Cards from './components/Cards.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld, 
+    Cards
+  }
+}
+</script>
+
+<style>
+body {
+  background-color: #f9f5e3;
+}
+
+img {
+  width: 200px;
+  height: 200px;
+  object-fit: cover;
+  margin-bottom: 2rem;
+  margin-top: 2rem;
+}
+
+.text {
+  position: absolute;
+  height: 200px;
+  width: 200px;
+  top: 12%;
+  bottom: 0;
+  left: 23.81%;
+  right: 0;
+  background: rgba(29, 154, 91, 0.72);
+  color: #fff;
+  visibility: hidden;
+  opacity: 0;
+  transition: opacity 0.2s, visibility 0.2s;
+}
+
+.img:hover .text {
+  visibility: visible;
+  opacity: 1;
+}
+.img .text p {
+  margin-top: 85px;
+  text-align: center;
+}
+
+</style>
---
+++ src/components/Cards.vue
@@ -0,0 +1,87 @@
+<template>
+<div>
+<h1 class="display-1 text-center"> 🦴 Dogs 🐕</h1>
+
+<div class="container text-center">
+  <div class="row">
+
+      <div class="col-sm-12 col-md-6 col-lg-4 img" v-for="pet in pets" :key="pet.name">
+        <img :src= pet.img />
+        <div class="overlay">
+          <div class="text">
+            <p>{{ pet.breed }}</p>
+            </div>
+        </div>
+      </div>
+      
+
+  </div>
+</div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Cards',
+  data:function(){
+      return {
+          pets: [
+ {
+    id: 1,
+    name: 'Rocky',
+    breed: 'Dalmatian',
+    color: 'White and Black',
+    size: 'Large',
+    img: 'https://i.imgur.com/dEMxUqK.jpg',
+    owner_id: 1
+  },
+  {
+    id: 2,
+    name: 'Skippy',
+    breed: 'Chinese Shar Pei',
+    color: 'Cream',
+    size: 'Medium',
+    img: 'https://i.imgur.com/ob0qRSz.jpg',
+    owner_id: 1
+  },
+  {
+    id: 3,
+    name: 'Biscuit',
+    breed: 'Maltese Shih Tzu',
+    color: 'White',
+    size: 'Small',
+    img: 'https://i.imgur.com/amBCLmT.png',
+    owner_id: 2
+  },
+  {
+    id: 4,
+    name: 'Zeke',
+    breed: 'Great Dane',
+    color: 'Black',
+    size: 'Large',
+    img: 'https://i.imgur.com/o2ttnds.jpg',
+    owner_id: 3
+  },
+  {
+    id: 5,
+    name: 'Booker',
+    breed: 'Goldendoodle',
+    color: 'Gold',
+    size: 'Medium',
+    img: 'https://i.imgur.com/v3tepaM.jpg',
+    owner_id: 4
+  },
+  {
+    id: 5,
+    name: 'Max',
+    breed: 'West Highland White Terrier',
+    color: 'White',
+    size: 'Small',
+    img: 'https://i.imgur.com/VaGS1A0.jpg',
+    owner_id: 4
+  }
+    ]
+      }
+  }
+}
+</script>
