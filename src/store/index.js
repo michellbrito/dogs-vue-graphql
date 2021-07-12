7d6ffccf1fc39880705a5038081f498b81a6da8f
Michell Brito <michellbrito@michells-mbp.myfiosgateway.com>
2021-07-12 15:20:33 -0400

    new owners state

--- src/store/index.js
+++ src/store/index.js
@@ -7,16 +7,20 @@ import axios from "axios";
 export const store = new Vuex.Store({
   state: {
     pets: [],
+    owners: []
   },
   mutations: {
     SET_PETS(state, pets) {
       state.pets = pets;
     },
+    SET_OWNERS(state, owners) {
+      state.owners = owners;
+    },
   },
   actions: {
     getPosts({ commit }) {
-        const fullUrl = `${window.location.href}graphql?query={pets{name,breed,img}}`;
-        // `http://localhost:3000/graphql?query={pets{name,breed,img}}`
+        // const fullUrl = `${window.location.href}graphql?query={pets{name,breed,img}}`;
+        const fullUrl = `http://localhost:3000/graphql?query={pets{name,breed,img}}`
       axios
         .get(fullUrl)
         .then((response) => {
@@ -24,14 +28,25 @@ export const store = new Vuex.Store({
         });
     },
     getLargeDogs({ commit }) {
-        const fullUrl = `${window.location.href}graphql?query={pets{name,breed,img,size}}`;
-        // `http://localhost:3000/graphql?query={petß∂ss{name,breed,img}}`
+        // const fullUrl = `${window.location.href}graphql?query={pets{name,breed,img,size}}`;
+        const fullUrl = `http://localhost:3000/graphql?query={pets{name,breed,img,size}}`
         axios
           .get(fullUrl)
           .then((response) => {
               const data = response.data.data.pets.filter(dog => dog.size === "Large");
               console.log(data)
             commit("SET_PETS", data);
+          });
+      },
+      getOwners({ commit }) {
+        // const fullUrl = `${window.location.href}graphql?query={owners{first_name,last_name}}`;
+        const fullUrl = `http://localhost:3000/graphql?query={owners{first_name,last_name,img}}`
+        axios
+          .get(fullUrl)
+          .then((response) => {
+              const data = response.data.data.owners;
+              console.log(data)
+            commit("SET_OWNERS", data);
           });
       },
   },
